8af70d8c0be5e8d303deaae8aaad36e32bbc9583
tdyachenko <86686484+tdyachenko@users.noreply.github.com>
2023-02-28 14:32:25 -0500

    ga

--- BAHM_2023/gtag.js
+++ BAHM_2023/gtag.js
@@ -1,12 +1,12 @@
 $(() => {
 
-  /* Default installation */
-
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-2P4LB5T5CJ"></script>
+<script>
   window.dataLayer = window.dataLayer || [];
-  function gtag() {
-    dataLayer.push(arguments);
-  };
+  function gtag(){dataLayer.push(arguments);}
   gtag('js', new Date());
+
   gtag('config', 'G-2P4LB5T5CJ');
+</script>
 
 });
