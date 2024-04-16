902d7d40d4396351fe74eb2793b31b430a6503e1
Tatiana Dyachenko <86686484+tdyachenko@users.noreply.github.com>
2024-04-15 21:57:46 -0400

    Update LinesToLoad_BAHM.R

--- LinesToLoad_BAHM.R
+++ LinesToLoad_BAHM.R
@@ -1,15 +1,15 @@
 
 # The first line is only if this is your first time to load the application.
 
-# In RStudio:   Move the cursor to the command line and click "Run" bottom 
-# ??? In R console: copy and paste the line and click Enter
+## Install needed libraries 
+install.packages(c("shiny", "bayesm", "HDInterval", "coda", "gtools","dplyr", "readr", "DT", "future", "promises", "ipc", "future.callr",
+                   "future.apply", "data.table", "shinyWidgets", "devtools","htmltools", "shinydashboard","shinyjs"))
 
-devtools::install_github("tdyachenko/BAHM-Mediation", ref = "r-pkg")
+## Getting the package from github
+devtools::install_github("tdyachenko/BAHM-Mediation", ref = "r-pkg",force = TRUE)
 
 # select 1 if you'd like to update the packages (recommended)
 # select 3 is no updates are requested
-
-# Select the lines below and click "Run"
 
 library(bahm)
 run_bahm()
